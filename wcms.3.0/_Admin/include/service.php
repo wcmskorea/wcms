ce3afa8dd85aaa5f7a1f2e6b74fef2b5dee2d029
WCMS in 10억홈피 <wcmskorea@gmail.com>
2016-10-19 06:03:57 +0900

    update
    update

---
+++ wcms.3.0/_Admin/include/service.php
@@ -0,0 +1,6 @@
+<?php
+require_once "../../_config.php";
+if(!defined('__CSS__')) header("Location: ./login.php");
+
+echo '<p class="small_gray center pd10">10억홈피닷컴의 서비스 이용정보를 보시려면 고객센터로 문의하시기 바랍니다.</p>';
+?>
